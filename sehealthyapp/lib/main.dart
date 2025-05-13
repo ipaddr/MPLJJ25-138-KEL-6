4cc0be800876016f8ab315e7f973672d78fec4ff
Abdul Hafiz <abdulhafiz.contact@gmail.com>
2025-05-13 14:37:13 +0700

    firebase set

--- sehealthyapp/lib/main.dart
+++ sehealthyapp/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // file ini otomatis dibuat saat setup Firebase CLI
 import 'screens/first_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const MyApp());
 }
 
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFEFF6FF),
         useMaterial3: true,
       ),
-      home: const FirstScreen(), // Tampilan awal sekarang FirstScreen
+      home: const FirstScreen(),
     );
   }
 }
